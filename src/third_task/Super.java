93c65525b4101245b779e0582a4f3f495e9f8e25
Peter James <Joyukr@ukr.net>
2015-12-19 15:19:27 +0200

    All task with lectures two (new version)

--- src/third_task/Super.java
+++ src/third_task/Super.java
@@ -4,70 +4,109 @@ package third_task;
 public class Super {
 
     public static void main(String[] args) {
-        //test2
+
+        //test1
         int speed = 60;
         int maxSpeed = 110;
+        int countPeople = 400;
+        int countFriend = 12;
+        int countVolunteer = 200;
+        String where = "Контрактова площа";
+        String when = "26.04.2015";
         boolean YouHavePrava = true;
-        superSpeed(speed, maxSpeed,YouHavePrava);
-        //test1
+
+        //test2
         int first = 33;
         int second = 32;
-        count(first, second);
-        System.out.println(ternaryCount(first, second));
-        //test3
-        int ageGirl = 20;
-        Years(ageGirl);
+
         //test4
-        first("Hello, U-Rise!");
+        int test = 5; //Введіть номер тесту
+
         //test5
         int count = 1;
-        while_go(count);
+
+
+        //test3
+        switch (test) {
+
+            case 1:
+                count(first, second);
+                System.out.println(ternaryCount(first, second));
+                break;
+            case 2:
+                superSpeed(speed, maxSpeed,YouHavePrava,countPeople,countFriend,countVolunteer,where,when);
+                break;
+            case 3:
+                System.out.println("Я і є test3");
+                break;
+            case 4:
+                first("Hello, U-Rise!");
+                break;
+            case 5:
+                while_go(count);
+                break;
+
+        }
+
     }
 
-    private static void superSpeed(int speed, int maxSpeed, boolean YouHavePrava) {
+    public static void superSpeed(int speed, int maxSpeed, boolean YouHavePrava, int countPeople,
+                                   int countFriend, int countVolunteer, String where, String when) {
         if (speed > maxSpeed || !YouHavePrava) {
             System.out.println("Астанавітесь!!!");
         } else {
             System.out.println("Гарної дороги");
         }
+
+        if (countPeople >= 333) {
+            System.out.println("Я бігтиму півмарафон");
+        } else {
+            System.out.println("Та ладно...всеодно побіжу");
+        }
+
+        if (countFriend !=0) {
+            System.out.println("Я бігтиму півмарафон з друзями, їх буде " + countFriend);
+        } else {
+            System.out.println("Доведеться бігти без друзів");
+        }
+
+        if (countVolunteer <=200) {
+            System.out.println("Організація півмарафону чудова ");
+        } else {
+            System.out.println("Потрібно більше волонтерів");
+        }
+
+        if (where == "Контрактова площа" && when == "26.04.2015") {
+            System.out.println("Я бігтиму по знайомому маршруту");
+        } else {
+            System.out.println("Я бігтиму по новому маршруту");
+        }
+
+
+
     }
 
-    private static String ternaryCount(int first, int second) {
-        int result = first / second;
-        return result == 5 ? "Ok" : "Learn";
-    }
+    public static String  ternaryCount(int first, int second) {
+        int result = first * second * 12;
+        String str = result == 5 ? "result = 5" : "result != 5";
+        return str;
 
-    private static void count(int first, int second) {
+        }
 
-        int result = first * second;
+    public static void count(int first, int second) {
+
+        int result = first * second * 12;
 
         if (result == 5) {
-            System.out.println("All right");
+            System.out.println("result = 5");
         } else {
-            System.out.println("Try again");
-        }
-
-    }
-
-    private static void Years(int ageGirl) {
-
-        switch (ageGirl) {
-
-            case 18:
-                System.out.println("I think you yet a little girl"); //Sorry for my english)
-                break;
-            case 19:
-                System.out.println("I think you pretty girl");
-                break;
-            case 20:
-                System.out.println("I think you good-looking");
-                break;
+            System.out.println("result != 5");
         }
 
 
     }
 
-    private static void first(String str) {
+    public static void first(String str) {
 
         int persent = 100;
         double happiness = 0.99;
@@ -81,7 +120,7 @@ public class Super {
 
     }
 
-    private static void while_go(int count) {
+    public static void while_go(int count) {
         while (count <= 10) {
             System.out.println(count);
             count++;
