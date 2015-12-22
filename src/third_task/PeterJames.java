579354ebe94a5229c2b5e0da79eeabba51175ad9
Peter James <Joyukr@ukr.net>
2015-12-22 13:54:03 +0200

    three task

--- src/third_task/PeterJames.java
+++ src/third_task/PeterJames.java
@@ -5,7 +5,10 @@ public class PeterJames {
 
     public static void main(String[] args) {
 
-        hello("Hello");
+        hello("Hello");//1
+        System.out.println(checkInt("@"));//2
+        tryGipotenysa();//3
+
 
     }
 
@@ -18,6 +21,31 @@ public class PeterJames {
         char ch4 = hello.charAt(4);
 
         System.out.println(ch + " " + ch1 + " " + ch2 + " " + ch3 + " " + ch4);
+
+    }
+
+    private static boolean checkInt(String string) {
+
+        if (string == null) return false;
+        return true;
+
+    }
+
+    private static void tryGipotenysa() {
+
+        int first = 3;
+        int second = 4;
+        int third = 5;
+
+        int katet1 = (int) Math.pow(first, 2);
+        int katet2 = (int) Math.pow(second, 2);
+        int result = (int) Math.pow(third, 2);
+
+        if (katet1 + katet2 == result) {
+            System.out.println("Трикутник прямокутний");
+        } else {
+            System.out.println("Трикутник якійсь інший");
+        }
 
     }
 
