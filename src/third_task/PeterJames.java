059d1bb7a53dbd9d4ccf030f4d65f7fd27d80943
Peter James <Joyukr@ukr.net>
2015-12-29 23:52:32 +0200

    new version when after i know what i stay here on new year(

--- src/third_task/PeterJames.java
+++ src/third_task/PeterJames.java
@@ -4,20 +4,19 @@ package third_task;
 public class PeterJames {
 
     public static void main(String[] args) {
-
         hello("Say Hello right now:");//1
-        System.out.println(checkInt("1"));//2
-        tryGipotenysa();//3
-        checkRivnobedreny();//4
-        checkTwoRivnostoronuh();//5
+        checkInt();//2
+        tryGipotenysa(3,4,5);//3
+        checkRivnobedreny(10,10,8);//4
+        checkTwoRivnostoronuh(17,17,17,23,23,23);//5
         twoCount(12,23);//6
         evenNumber(17,23);//7
-        fooCount();//8
-        comeBack();//9
-        fooCountPartTwo();//10
-        whyBig();//11
+        fooCount(1723);//8
+        comeBack(1723);//9
+        fooCountPartTwo(1723);//10
+        whyBig(1723);//11
         quadraticEquation();//12 and 13
-        System.out.println(checkString("We love Ukraine"));//14
+        System.out.println(checkString("We love Ukraine"));//14*/
     }
 
     private static void hello(String hello) {
@@ -27,74 +26,69 @@ public class PeterJames {
 
     }
 
-    private static boolean checkInt(String string) {
-
-        if (string == null) return false;
-        return string.matches("^-?\\d+$");
-    }
-
-    private static void tryGipotenysa() {
-
-        int first = 3;
-        int second = 4;
-        int third = 5;
-
-        int katet1 = (int) Math.pow(first, 2);
-        int katet2 = (int) Math.pow(second, 2);
-        int result = (int) Math.pow(third, 2);
-
-        if (katet1 + katet2 == result) {
-            System.out.println("Трикутник прямокутний");
-        } else {
-            System.out.println("Трикутник якійсь інший");
-        }
+    private static void checkInt() {
 
     }
 
-    private static void checkRivnobedreny() {
+    private static void tryGipotenysa(int first, int second, int third) {
 
-        int storona1 = 10;
-        int storona2 = 10;
-        int storona3 = 8;
+        if (first + second > third) {
+            int katet1 = (int) Math.pow(first, 2);
+            int katet2 = (int) Math.pow(second, 2);
+            int result = (int) Math.pow(third, 2);
 
-        if (storona1 == storona2 && storona3 < storona1 || storona3 < storona2 && storona3 >0) {
-            System.out.println("Трикутник рівнобедрений");
+            if (katet1 + katet2 == result) {
+                System.out.println("Трикутник прямокутний");
+            } else {
+                System.out.println("Трикутник якійсь інший");
+            }
         } else {
-            System.out.println("Трикутник не рівнобедрений");
+            System.out.println("Це не трикутник");
         }
-
     }
 
-    private static void checkTwoRivnostoronuh() {
+    private static void checkRivnobedreny(int storona1, int storona2, int storona3) {
 
-        int one = 12;
-        int two = 12;
-        int thee = 12;
-        String  result = null;
+        if (storona1 + storona2 > storona3) {
 
-        int  foo = 23;
-        int five = 23;
-        int six = 23;
-        String result2 = null;
-
-        if (one == two && thee == (two + one + thee) / 3) {
-            result = "We love Ukraine";
+            if (storona1 == storona2 && storona3 < storona1 || storona3 < storona2 && storona3 > 0) {
+                System.out.println("Трикутник рівнобедрений");
+            } else {
+                System.out.println("Трикутник не рівнобедрений");
+            }
         } else {
-            result = "Океан Ельзи";
+            System.out.println("Це не трикутник");
         }
+    }
 
-        if (foo == five && six == (five + foo + six) / 3) {
-            result2 = "We love Ukraine";
+    private static void checkTwoRivnostoronuh(int one,int two,int three,int foo,int five,int six) {
+
+        if (one + two > three && foo + five > six) {
+
+            String result = null;
+
+            String result2 = null;
+
+            if (one == two && three == (two + one + three) / 3) {
+                result = "We love Ukraine";
+            } else {
+                result = "Океан Ельзи";
+            }
+
+            if (foo == five && six == (five + foo + six) / 3) {
+                result2 = "We love Ukraine";
+            } else {
+                result2 = "Hello";
+            }
+
+            if (result == result2) {
+                System.out.println("Вітаю ми маємо два рівносторонніх трикутника");
+            } else {
+                System.out.println("Що ж теж вітаю, але в нас не два рівносторонніх трикутника");
+            }
         } else {
-            result2 = "Hello";
+            System.out.println("Це не два трикутника");
         }
-
-        if (result == result2) {
-            System.out.println("Вітаю ми маємо два рівносторонніх трикутника");
-        } else {
-            System.out.println("Що ж теж вітаю, але в нас не два рівносторонніх трикутника");
-        }
-
     }
 
     private static void twoCount(int first_cool, int second_cool) {
@@ -109,26 +103,42 @@ public class PeterJames {
     private static boolean evenNumber(int first, int second) {
 
         if (first % 2 == 0 && second % 2 == 0) {
-           return true;
-        } else
+            return true;
+        } else {
             return false;
+        }
     }
 
-    private static boolean fooCount() {
+    private static void fooCount(int girls) {
 
-        int mas[] = {3,3,3,3};
+        int arr[] = new int[4];
 
-        int result = mas[0] + mas[1] + mas[2] + mas[3];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = girls % 10;
+            girls/=10;
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            result += arr[i];
+        }
 
         if (result % 2 == 0) {
-            return true;
-        } else
-            return false;
+            System.out.println("Сума парна");
+        } else {
+            System.out.println("Сума не парна");
+        }
     }
 
-    private static void comeBack() {
+    private static void comeBack(int chec) {
 
-        int mas[] = {1, 7, 2, 3, 2};
+        int mas[] = new int[4];
+
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = chec % 10;
+            chec/=10;
+        }
 
         String check = null;
 
@@ -140,12 +150,16 @@ public class PeterJames {
             }
         }
         System.out.println(check);
-
     }
 
-    private static void fooCountPartTwo() {
+    private static void fooCountPartTwo(int part) {
 
-        int mas[] = {17, 23, 17, 23};
+        int mas[] = new int[4];
+
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = part % 10;
+            part/=10;
+        }
 
         int result = 0;
         int result2 = 0;
@@ -158,12 +172,16 @@ public class PeterJames {
         } else {
             System.out.println("Сума половини чисел не одинакові");
         }
-
     }
 
-    private static void whyBig() {
+    private static void whyBig(int big) {
 
-        int mas[] = {7, 3, 2, 1};
+        int mas[] = new int[4];
+
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = big % 10;
+            big/=10;
+        }
 
         int count = mas[0];
         int count1 = mas[1];
@@ -175,7 +193,6 @@ public class PeterJames {
         } else {
             System.out.println("Дивись на два рядки вище, але буде навпаки");
         }
-
     }
 
     private static void quadraticEquation() {
@@ -197,7 +214,6 @@ public class PeterJames {
         if (x1 % 2 == 0 | x2 % 2 == 0) {
             System.out.println("Don't worry!!! Один із коренів є парним");
         }
-
     }
 
     private static boolean checkString(String string) {
@@ -206,7 +222,6 @@ public class PeterJames {
         return string.matches("^-?\\d+$");
 
     }
-
 }
 
 
