7378a6710a24a05f5938f8416bb3d73eb8db4b36
Igor <joyukr@ukr.net>
2016-01-07 21:45:09 +0200

    07.01.2015 lectures

--- src/third_task/Start4.java
+++ src/third_task/Start4.java
@@ -1,13 +1,25 @@
 package third_task;
 
+/*створити рекурсивний метод для функції піднесення числа в степінь
+
+зробити алгоритм фибоначи в звичайній (циклом) формі та рекурсивно
+* */
+
 
 public class Start4 {
 
     public static void main(String[] args) {
 
-        int mas[] = {2, 5, 4, 9, 4, 6};
+        int mas[] = {2, 5, 4, 9, 4, 8};
 
         System.out.println(sumMas(mas));
+
+        System.out.println(fib(10));
+
+        System.out.println(fibRec(12));
+
+        System.out.println(ret(2,4));
+
     }
 
     private static int sumMas(int mas[]) {
@@ -15,9 +27,44 @@ public class Start4 {
         int result = 0;
 
         for (int i = 0; i < mas.length; i++) {
-             result += mas[i];
+            result += mas[i];
         }
         return result;
     }
 
+    private static int fib(int arr) {
+
+        int mas[] = new int[arr + 1];
+        mas[0] = 0;
+        mas[1] = 1;
+
+        for (int i = 2; i < mas.length; i++) {
+            mas[i] = mas[i - 2] + mas[i - 1];
+        }
+        return mas[arr];
+    }
+
+
+    private static int fibRec(int arr) {
+
+        return (arr < 2) ? arr : fibRec(arr - 2) + fibRec(arr - 1);
+    }
+
+    private static int ret(int arg, int count) {
+
+        if (count > 0 && count < 2) {
+            return arg;
+        }
+        if (count == 2) {
+            return arg * arg;
+        }else {
+            int res = arg * arg;
+            if (count != 0) {
+                res = res * arg;
+                ret(arg, count - 1);
+            }
+            return res;
+        }
+    }
 }
+
