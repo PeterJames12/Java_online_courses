11a57770614db0eced0a50ec98facea1445ed318
Igor <joyukr@ukr.net>
2016-01-08 15:54:58 +0200

    08.01.2015 new version (all) task with lectures

--- src/third_task/Start4.java
+++ src/third_task/Start4.java
@@ -5,6 +5,11 @@ public class Start4 {
 
     public static void main(String[] args) {
 
+        int first = 5;
+        int second = 4;
+        final int sex = first;
+        test(first, second, sex);
+
         int mas[] = {2, 5, 4, 9, 4, 8};
 
         System.out.println(sumMas(mas));
@@ -13,8 +18,16 @@ public class Start4 {
 
         System.out.println(fibRec(12));
 
-        System.out.println(ret(2,4));
+    }
 
+    private static void test(int ars, int wer,int result) {
+
+        if (wer > 1) {
+            ars *= result;
+            test(ars, wer - 1,result);
+        } else {
+            System.out.println(ars);
+        }
     }
 
     private static int sumMas(int mas[]) {
@@ -39,27 +52,11 @@ public class Start4 {
         return mas[arr];
     }
 
-
     private static int fibRec(int arr) {
 
         return (arr < 2) ? arr : fibRec(arr - 2) + fibRec(arr - 1);
     }
-
-    private static int ret(int arg, int count) {
-
-        if (count > 0 && count < 2) {
-            return arg;
-        }
-        if (count == 2) {
-            return arg * arg;
-        }else {
-            int res = arg * arg;
-            if (count != 0) {
-                res = res * arg;
-                ret(arg, count - 1);
-            }
-            return res;
-        }
-    }
 }
+
+
 
