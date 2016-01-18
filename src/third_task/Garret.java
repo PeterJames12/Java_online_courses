4fa7cb790773f3d500e215dcdcdb10fc26816e66
Igor <joyukr@ukr.net>
2016-01-18 18:28:26 +0200

    18.01.2016 second version homework

--- src/third_task/Garret.java
+++ src/third_task/Garret.java
@@ -1,20 +1,91 @@
 package third_task;
 
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Garret {
 
     public static void main(String[] args) {
+        System.out.println(fib(10));//15
+        System.out.println(fibRec(12));//16
 
-        System.out.println(fib(10));
-        System.out.println(fibRec(12));
+        System.out.println(fec(5));//18
+        System.out.println(fecNearly(6));//17
+        zeroOne(255);//19
 
-        System.out.println(fec(5));
-        System.out.println(fecNearly(6));
-        zeroOne(255);
+        freeCount(12);
+        copyMac();
+        secondCount(5,12);
+    }
+
+    private static void secondCount(int first, int second) {
+
+        int mac[] = new int[second - first - 1];
+
+        int res;
+
+        for (int i = 0; i < mac.length; i++) {
+            mac[i] = ++first;
+            res = mac[i] % 2;
+            if (res == 0) {
+                System.out.print(" " + mac[i]);
+            }
+        }
+    }
+
+    private static void freeCount(int number) {
+
+        int mas[] = new int[number];
+
+        Random random = new Random();
+
+        for (int i = 0; i < mas.length; i++) {
+            int goMas = random.nextInt(23);
+            mas[i] = goMas;
+        }
+        goConsoleMas(mas);
+
+        goSearshMas(mas);
+    }
+
+    private static void copyMac() {
+
+        int mas[] = {1, 2, 5, 4, 6, 7, 8, 5, 8, 2, 5};
+
+        int arr[] = new int[5];
+
+        System.arraycopy(mas, 0, arr, 0, arr.length);
+
+        System.out.println(Arrays.toString(mas));
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void goSearshMas(int[] mas) {
+
+        int find = 12;
+        int result = 0;
+
+        for (int i = 0; i < mas.length; i++) {
+            int elem = mas[i];
+            if (elem == find) {
+                result = i;
+                break;
+            } else {
+                result = -1;
+            }
+        }
+        System.out.println("Index " + result);
+    }
+
+    private static void goConsoleMas(int mas []) {
+
+        System.out.println(Arrays.toString(mas));
 
     }
 
     private static void withDouble(double router) {
+
         double db = 0.625;
         double result;
         result = db * 2;
@@ -45,7 +116,7 @@ public class Garret {
         return mas[arr];
     }
 
-    private static int fibRec(int arr) {
+    private static long fibRec(int arr) {
 
         return (arr < 2) ? arr : fibRec(arr - 2) + fibRec(arr - 1);
     }
@@ -78,7 +149,8 @@ public class Garret {
             return cisco;
         }
         while (cisco > 0) {
-            res *= cisco--;
+            res = res * cisco;
+            cisco--;
         }
         return res;
     }
